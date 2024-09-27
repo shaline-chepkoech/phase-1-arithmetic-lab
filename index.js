dfce8da4e0220368eb353548beba63cbfa0e2526
shaline-chepkoech <chepkoechshaline726@gmail.com>
2024-09-27 20:30:27 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() *100) + 1;
+
+const num3 = 16;
+const num4 = 12;
+const mod = num3 % num4;
+
+const num5 = 5;
+const num6 = 10;
+const num7 = 15;
+const num8 = 20;
+const max = Math.max(num5, num6, num7, num8);
